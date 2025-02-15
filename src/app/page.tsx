ee1db30d97b9e38c0627fd127e2605e61d91bb52
md-maruf-billa <157400283+md-maruf-billa@users.noreply.github.com>
2025-02-15 16:15:36 +0600

    Added-: added initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+      Hi every one
+    </div>
+  );
+};
+
+export default HomePage;
